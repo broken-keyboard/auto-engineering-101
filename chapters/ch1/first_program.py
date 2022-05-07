64057cde05ad4341d3e3584a357dfe7da768bebf
broken-keyboard <kabutogari@gmail.com>
2022-05-07 23:40:13 +0900

    Challenge 1

---
+++ chapters/ch1/first_program.py
@@ -0,0 +1,4 @@
+first_name = input('Enter your first name')
+last_name = input('Enter your last name')
+fav_animal = input('What is your favourite animal?')
+print(f"{first_name} {last_name}'s favourite animal is a {fav_animal}")
