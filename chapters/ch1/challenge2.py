5b1cdbbf786abf54b1e17e6795aeb68624960b0f
broken-keyboard <kabutogari@gmail.com>
2022-05-07 23:45:37 +0900

    The second practice in the series of automation

---
+++ chapters/ch1/challenge2.py
@@ -0,0 +1,4 @@
+adjective = input('Enter an adjective')
+season = input('Enter a season')
+number = input('Enter a number')
+print(f"On a {adjective} {season} day, I drink at least {number} cups of juice!")
